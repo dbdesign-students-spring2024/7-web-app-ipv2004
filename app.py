95d4d9250d25ad4ff09a00379a93d9d7471c5a1d
ipv2004 <157513436+ipv2004@users.noreply.github.com>
2024-04-16 16:37:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ sentry_sdk.init(
     dsn=os.getenv("SENTRY_DSN"),
     # enable_tracing=True,
     # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
-    traces_sample_rate=1.0,
     # Set profiles_sample_rate to 1.0 to profile 100% of sampled transactions.
     # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
